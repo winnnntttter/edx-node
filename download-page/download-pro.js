13d88acff7d692b0969bf175ec5cc72d0563e42a
DanteMaycry <1013222027@qq.com>
2017-12-01 22:54:36 +0800

    下载页面优化

---
+++ download-page/download-pro.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const uuidV1 = require('uuid/v1');
+
+const downloadPage = (url = 'http://nodeprogram.com') =>{
+  console.log('downloading',url);
+  const fetchPage = (urlF,callback) =>{
+    http.get(urlF,(response)=>{
+      let buf = '';
+      response.on('data',(chunk)=>{
+        buf +=chunk;
+      })
+      response.on('end',()=>{
+        callback(null,buf);
+      })
+      response.on('error',(error)=>{
+        console.error(`Got error:${error.message}`);
+      })
+    }).on('error',(error)=>{
+      console.error(`Got error:${error.message}`);
+    })
+  }
+  const folderName = uuidV1();
+  fs.mkdirSync(folderName);
+  fetchPage(url,(error,data)=>{
+    if(error) console.error(error);
+    fs.writeFileSync(path.join(__dirname,folderName,'url.txt'),url);
+    fs.writeFileSync(path.join(__dirname,folderName,'url.html'),data);
+    console.log('downloading is done in folder',folderName);
+  })
+}
+
+downloadPage(process.argv[2]);
