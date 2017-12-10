564b7d8e34be838e515770d031387a1dcbc1bfab
DanteMaycry <1013222027@qq.com>
2017-12-10 19:25:34 +0800

    解决神奇bug，datajson中的格式问题

--- express/blog3/routes/index.js
+++
@@ -1,4 +0,0 @@
-const posts = require('./posts')
-const comments = require('./comments')
-
-module.exports = {posts, comments}
--- express/blog3/store.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// seed store data by reading from disk
-const storeData = fs.readFileSync('./data.json', "utf8")
-  .replace(/\r?\n|\r/g,'\n'); // normalize line endings
-
-module.exports = JSON.parse(storeData)
