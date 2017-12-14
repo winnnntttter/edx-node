b896952a4579708f43da7877336f7c7869218057
DanteMaycry <1013222027@qq.com>
2017-12-14 22:53:19 +0800

    mongodb实际应用

---
+++ crud-mongo-rest-api/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const logger = require('morgan');
+const errorhandler = require('errorhandler');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+const url = 'mongodb://localhost:27017/edxCourseDb'
+let app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+mongodb.MongoClient.connect(url,(error,db)=>{
+  if(error) return process.exit(1);
+
+  app.get('/accounts',(req,res)=>{
+    db.collection('accounts')
+    .find({},{sort:{_id:-1}})
+    .toArray((error,accounts)=>{
+      if(error) return next(error);
+      res.send(accounts);
+    })
+  })
+
+  app.post('/accounts',(req,res)=>{
+    let newAccount = req.body;
+    db.collection('accounts').insert(newAccount,(error,results)=>{
+      if(error) return next(error)
+      res.send(results)
+    })
+  })
+
+  app.put('/accounts/:id',(req,res)=>{
+    db.collection('accounts')
+    .update({_id:mongodb.ObjectID(req.params.id)},
+      {$set:req.body},
+      (error,results)=>{
+        if(error) return next(error)
+        res.send(results)
+      }
+    )
+  })
+
+  app.delete('/accounts/:id',(req,res)=>{
+    db.collection('accounts')
+    .remove({_id:mongodb.ObjectID(req.params.id)},(error,results)=>{
+      if(error) return next(eror)
+      res.send(results)
+    })
+  })
+
+  app.listen(3000)
+})
