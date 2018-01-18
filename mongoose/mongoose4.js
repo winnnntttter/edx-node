cbcab858ae9f957a423c5f4d966a350965eb8130
zhulinjie <jiezeal@foxmail.com>
2018-01-18 10:10:53 +0800

    virtual field

--- mongoose/mongoose4.js
+++ mongoose/mongoose4.js
@@ -5,6 +5,7 @@ mongoose.connect("mongodb://localhost:27017/test4",{useMongoClient:true});
 
 const bookSchema = mongoose.Schema({
   name:String,
+  author:String,
   published:Boolean,
   createdAt:Date,
   updateAt:{type:Date,default:Date.now()},
@@ -12,19 +13,22 @@ const bookSchema = mongoose.Schema({
   reviews:[mongoose.Schema.Types.Mixed]
 });
 
-//虚拟document
+//虚拟字段
 bookSchema.virtual("authorPhotoUrl").get(function(){//不能使用箭头函数，否则该函数会在myBook.save执行后才执行
-  console.log(this);
   if(!this.email) return null;
   var crypto = require("crypto");
   email = this.email;
   email = email.trim();
   email = email.toLowerCase();
   var hash = crypto.createHash("md5").update(email).digest("hex");
-  var baseUrl = "https://secure.gravatar.com/avatar/";
-  return baseUrl + hash;
+  var baseUrl = "https://s.gravatar.com/avatar/";
+  var para = "?s=800";
+  return baseUrl + hash + para;
 });
-
+bookSchema.virtual("virtualField").get(function(){
+  if(!this.name || !this.author) return null;
+  return this.name + ":" + this.author;
+});
 
 let Book = mongoose.model("Book",bookSchema);
 let myBook = new Book({
@@ -42,6 +46,7 @@ myBook.save((err,results)=>{
   }else{
     console.log(`saved: ${results}`);
     console.log(`Author photo: ${myBook.authorPhotoUrl}`);
+    console.log(`virtual field:${myBook.virtualField}`);
     myBook.remove(process.exit);
   }
 });
